b81f3c5f6b4bee2294c04a1953d9ff805892203c
AlejandroJimenezMurciano <alejimenezmurciano@gmail.com>
2023-01-25 18:02:50 +0100

    wordle

--- src/js/index.js
+++ src/js/index.js
@@ -8,6 +8,7 @@ import '../scss/styles.scss';
 - importaciones
 - y constantes q necesites utilizar en el index
 */
+const userWordForm = document.getElementById('user-word-form');
 const wordle = 'hello';
 const gameBoardElement = document.getElementById('game-board');
 const guessRows = [
@@ -20,14 +21,20 @@ const guessRows = [
 let currentRow = 0;
 let currentTile = 0;
 
+//creando cada fila y cada cuadrado del wordle
 guessRows.forEach((guessRow, guessRowIndex) => {
-    const rowElement = document.createElement('div')
-    rowElement.setAttribute('id', 'guessRow-' + guessRowIndex)
-    guessRow.forEach((_guess, guessIndex) => {
-        const tileElement = document.createElement('div')
-        tileElement.setAttribute('id', 'guessRow-' + guessRowIndex + '-tile-' + guessIndex)
-        tileElement.classList.add('tile')
-        rowElement.append(tileElement)
-    });
-    gameBoardElement.append(rowElement)
-})
+  const rowElement = document.createElement('div');
+  rowElement.setAttribute('id', 'guessRow-' + guessRowIndex);
+  guessRow.forEach((_guess, guessIndex) => {
+    const tileElement = document.createElement('div');
+    tileElement.setAttribute(
+      'id',
+      'guessRow-' + guessRowIndex + '-tile-' + guessIndex
+    );
+    tileElement.classList.add('tile');
+    rowElement.append(tileElement);
+  });
+  gameBoardElement.append(rowElement);
+});
+
+//para hacer que al hacer intro se envie la palabra
