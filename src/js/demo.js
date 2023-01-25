7984fee7b3bf8ebf041fa76ef56de666e6b62572
AlejandroJimenezMurciano <alejimenezmurciano@gmail.com>
2023-01-25 15:31:18 +0100

    wordle

--- src/js/demo.js
+++ src/js/demo.js
@@ -1,22 +1,3 @@
 //const sayHello = () => console.log('HELLO');
 //export { sayHello };
-const gameBoardElement = document.getElementById('game-board');
-const allRows = [
-  ['', '', '', '', ''],
-  ['', '', '', '', ''],
-  ['', '', '', '', ''],
-  ['', '', '', '', ''],
-  ['', '', '', '', '']
-];
 
-allRows.forEach((row, rowIndex) => {
-  const row = document.createElement('div');
-  row.setAtribute('id', 'row-' + rowIndex);
-  row.forEach((boxElement, boxIndex) => {
-    const boxElement = document.createElement('div');
-    boxElement.setAtribute('id', 'row-' + rowIndex + 'box' + boxIndex);
-    row.append(gameBoardElement);
-    row.classList.add('box');
-  });
-  gameBoardElement.append(row);
-});
--- src/js/index.js
+++ src/js/index.js
@@ -8,3 +8,26 @@ import '../scss/styles.scss';
 - importaciones
 - y constantes q necesites utilizar en el index
 */
+const wordle = 'hello';
+const gameBoardElement = document.getElementById('game-board');
+const guessRows = [
+  ['', '', '', '', ''],
+  ['', '', '', '', ''],
+  ['', '', '', '', ''],
+  ['', '', '', '', ''],
+  ['', '', '', '', '']
+];
+let currentRow = 0;
+let currentTile = 0;
+
+guessRows.forEach((guessRow, guessRowIndex) => {
+    const rowElement = document.createElement('div')
+    rowElement.setAttribute('id', 'guessRow-' + guessRowIndex)
+    guessRow.forEach((_guess, guessIndex) => {
+        const tileElement = document.createElement('div')
+        tileElement.setAttribute('id', 'guessRow-' + guessRowIndex + '-tile-' + guessIndex)
+        tileElement.classList.add('tile')
+        rowElement.append(tileElement)
+    });
+    gameBoardElement.append(rowElement)
+})
